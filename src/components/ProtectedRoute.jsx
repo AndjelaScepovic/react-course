87db65c56b84d563594b4b99833f807006f97685
Andjela Scepovic <scepovic.andjela@gmail.com>
2024-11-09 18:29:01 +0100

    deleted a comment

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,9 +3,9 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext.jsx";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext); // Proverava korisnika iz AuthContext
+  const { user } = useContext(AuthContext);
 
-  return user ? children : <Navigate to="/home" />; // Ako korisnik nije ulogovan, ide na login
+  return user ? children : <Navigate to="/home" />;
 };
 
 export default ProtectedRoute;
